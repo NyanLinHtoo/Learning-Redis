0a8d13a7ad14010e0c088e03234415cb9479ec29
NyanLinHtoo <nyanlinhtoo.ygn@gmail.com>
2024-11-15 18:45:07 +0700

    learned sorted sets (done)

--- src/app/actions/create.js
+++ src/app/actions/create.js
@@ -9,6 +9,21 @@ export async function createBook(formData) {
   // create book id
   const id = Math.floor(Math.random() * 100000);
 
+  // add the book to the sorted set
+  // unique return 0 and 1
+  const unique = await client.zAdd(
+    "books",
+    {
+      value: title,
+      score: id,
+    },
+    { NX: true }
+  );
+
+  if (!unique) {
+    return { error: "That book has already been added" };
+  }
+
   // save new hash for the book
   await client.hSet(`books:${id}`, {
     title,
